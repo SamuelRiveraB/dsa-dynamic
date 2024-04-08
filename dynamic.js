50409699852612d2439d6ce9f86ca2d2dff6a2b5
Samuel <samuelrivba@gmail.com>
2024-04-08 18:16:52 -0500

    Add closures

--- dynamic.js
+++ dynamic.js
@@ -3,15 +3,17 @@ function addTo80(n) {
   return n + 80;
 }
 
-let cache = {};
 function memoizedAddTo80(n) {
-  if (n in cache) {
-    return cache[n];
-  } else {
-    console.log("long time");
-    cache[n] = n + 80;
-    return cache[n];
-  }
+  let cache = {};
+  return function (n) {
+    if (n in cache) {
+      return cache[n];
+    } else {
+      console.log("long time");
+      cache[n] = n + 80;
+      return cache[n];
+    }
+  };
 }
 
 console.log("1", memoizedAddTo80(5));
